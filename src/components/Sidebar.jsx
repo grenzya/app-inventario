5066480ebe932ba7a1fff43a3e69eae80f2554ec
VendoQuesito <benjamin.molina@alumnos.ucn.cl>
2025-07-06 16:51:37 -0400

    Fix: se hizo que la barra se cerrara al cambiar de pagina

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,16 @@
-import { NavLink } from "react-router-dom";
+import { useEffect } from "react";
+import { NavLink, useLocation } from "react-router-dom";
+import { FaHome, FaBoxes, FaTruck } from "react-icons/fa";
 
 function Sidebar({ isOpen, onClose }) {
+  const location = useLocation();
+
+  useEffect(() => {
+    if (isOpen) {
+      onClose();
+    }
+  }, [location.pathname]);
+
   return (
     <div
       style={{
@@ -9,13 +19,14 @@ function Sidebar({ isOpen, onClose }) {
         left: isOpen ? 0 : "-250px",
         width: "250px",
         height: "100%",
-        background: "#222",
-        color: "#fff",
-        paddingTop: "60px",
-        transition: "left 0.3s",
+        background: "#E5E5E5",
+        paddingTop: "80px",
+        transition: "left 0.35s",
         zIndex: 1000,
+        boxShadow: "4px 4px 4px rgba(0, 0, 0, 0.25)",
       }}
     >
+      {/* Botón de Cerrar */}
       <button
         onClick={onClose}
         style={{
@@ -23,29 +34,42 @@ function Sidebar({ isOpen, onClose }) {
           top: "10px",
           right: "10px",
           fontSize: "20px",
+          background: "transparent",
+          border: "none",
+          color: "#333",
+          cursor: "pointer",
         }}
       >
         ✕
       </button>
-      <ul style={{ listStyle: "none", padding: "20px" }}>
-        <li>
-          <NavLink to="/" style={{ color: "#fff", textDecoration: "none" }}>
+
+      <ul style={{ listStyle: "none", padding: "0 20px" }}>
+        <li style={{ marginBottom: "20px" }}>
+          <NavLink
+            to="/"
+            style={navLinkStyle}
+          >
+            <FaHome style={{ marginRight: "10px" }} />
             Inicio
           </NavLink>
         </li>
-        <li>
+
+        <li style={{ marginBottom: "20px" }}>
           <NavLink
             to="/productos"
-            style={{ color: "#fff", textDecoration: "none" }}
+            style={navLinkStyle}
           >
+            <FaBoxes style={{ marginRight: "10px" }} />
             Productos
           </NavLink>
         </li>
+
         <li>
           <NavLink
             to="/proveedores"
-            style={{ color: "#fff", textDecoration: "none" }}
+            style={navLinkStyle}
           >
+            <FaTruck style={{ marginRight: "10px" }} />
             Proveedores
           </NavLink>
         </li>
@@ -53,5 +77,17 @@ function Sidebar({ isOpen, onClose }) {
     </div>
   );
 }
+
+const navLinkStyle = {
+  display: "flex",
+  alignItems: "center",
+  padding: "10px 20px",
+  background: "#75D797",
+  borderRadius: "9999px",
+  textDecoration: "none",
+  color: "#000000",
+  boxShadow: "2px 2px 4px rgba(0,0,0,0.25)",
+  fontWeight: "500",
+};
 
 export default Sidebar;
